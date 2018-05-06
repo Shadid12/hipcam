da9b11af3214d440b39f474cbcbf4a80bdbf8306
shadid12 <shadidhaque2012@gmail.com>
2018-05-06 19:18:42 -0400

    added new camera stuff

--- src/components/CameraContainer.js
+++ src/components/CameraContainer.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import Webcam from 'react-webcam';
 
 export default class CameraContainer extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            img: ''
+        }
+    }
+
+    setRef = (webcam) => {
+        this.webcam = webcam;
+    }
+     
+    capture = () => {
+        const img = this.webcam.getScreenshot();
+        this.setState({ img: img })
+    };
+
     render() {
         return(
             <div>
-                Hello worlds
+                <Webcam
+                    audio={false}
+                    height={350}
+                    ref={this.setRef}
+                    screenshotFormat="image/jpeg"
+                    width={350}
+                />
+                <button onClick={this.capture}>Capture photo</button>
+                <img src={this.state.img} />>
             </div>
         )
     }
